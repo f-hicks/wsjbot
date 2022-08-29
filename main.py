2c19d5821dc029fc39395c49478140e7a65744b1
brozorb <brozorb.yt@gmail.com>
2022-08-29 17:07:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,12 @@ socials = {
 }
 
 events = {
-    "test event":"28 august 2022"
     "London trip": "1 October, 2022",
     "Whole unit fundraiser event": "12 November, 2022",
     "Joint christmas event with berkshire unit 69": "3 December, 2022",
     "Training camp 3": "20 January, 2023",
     "Training camp 4": "28 April, 2023",
-    "bag check": "8 July, 2023",
+    "Bag check": "8 July, 2023",
     "Jamboree Starts!": "1 august, 2023"
 }
 

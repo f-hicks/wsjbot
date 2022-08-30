6d64ca7cfb41191c1c34e6ced597aec6fffe7735
brozorb <84518177+brozorb@users.noreply.github.com>
2022-08-30 10:25:23 +0100

    Delete events.py

--- events.py
+++
@@ -1,1 +0,0 @@
-events = {'London trip': '1 October, 2022', 'Whole unit fundraiser event': '12 November, 2022', 'Joint christmas event with berkshire unit 69': '3 December, 2022', 'Training camp 3': '20 January, 2023', 'Training camp 4': '28 April, 2023', 'Bag check': '8 July, 2023', 'Jamboree Starts!': '1 august, 2023'}

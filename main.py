2bd2b1244cca27fb4007788e7c92527a81dd433d
brozorb <brozorb.yt@gmail.com>
2022-08-31 13:14:36 +0100

    update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from email.policy import default
+from ssl import Options
 from discord.ext import tasks
 from discord import default_permissions
+from discord.commands import Option
 import discord
 import os
 from dotenv import load_dotenv
 from datetime import datetime
 import random
 from events import events
+
+
 
 load_dotenv()
 
@@ -56,7 +61,7 @@ def daysuntilkorea():
 colours = {"red": discord.Colour.red(),"green": discord.Colour.green(),"blue": discord.Colour.blue(),"purple": discord.Colour.purple(),"magenta": discord.Colour.magenta(),"gold": discord.Colour.gold(),"orange": discord.Colour.orange(),"teal": discord.Colour.teal(),"dark_teal": discord.Colour.dark_teal(),"dark_green": discord.Colour.dark_green(),"dark_blue": discord.Colour.dark_blue(),"dark_purple": discord.Colour.dark_purple(),"dark_magenta": discord.Colour.dark_magenta(),"dark_gold": discord.Colour.dark_gold(),"dark_orange": discord.Colour.dark_orange(),"dark_red": discord.Colour.dark_red(),"lighter_grey": discord.Colour.lighter_grey(),"darker_grey": discord.Colour.darker_grey(),"light_grey": discord.Colour.light_grey(),"dark_grey": discord.Colour.dark_grey(),"black": discord.Colour.default(),"white": discord.Colour.from_rgb(255,255,255),"default": discord.Colour.default(),"blurple": discord.Colour.blurple(),"greyple": discord.Colour.greyple(),}
 
 
-bot = discord.Bot(debug_guilds=[947173792826941460])
+bot = discord.Bot(debug_guilds=[])
 discord.Intents.all()
 
 class channelview(discord.ui.View):
@@ -104,7 +109,75 @@ class addeventconfirmdate(discord.ui.View):
         await interaction.response.send_modal(addeventmodal(title="create an embed"))
         await interaction.delete_original_message()
 
+
+class editevent_(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        global events
+
+    
+    @discord.ui.button(label="Edit",style=discord.ButtonStyle.primary, custom_id="editevent")
+    async def editbutton_callback(self, button, interaction):
+        await interaction.response.edit_message(view=editsevent_())
+        #await interaction.delete_original_message()
+
+    @discord.ui.button(label="Delete",style=discord.ButtonStyle.red, custom_id="deleteevent")
+    async def deletebutton_callback(self, button, interaction):
+        events.pop(editevents)
+        #await interaction.delete_original_message()
+
+class editeventname(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        global events
         
+        self.add_item(discord.ui.InputText(label="please enter a title for the event", placeholder="title", custom_id="eventname"))
+    async def callback(self, interaction: discord.Interaction):
+        global events
+        oldeventname = editevents
+        neweventname = self.children[0].value
+        keys = list(events.keys())
+        index=keys.index(oldeventname)
+        date = events[oldeventname]
+        events.pop(oldeventname)
+        events = insert(events,{neweventname:date},index)
+        await interaction.response.edit_message(view=None)
+
+
+class editsevent_(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        global events
+    
+    @discord.ui.button(label="Edit name",style=discord.ButtonStyle.primary, custom_id="editeventname")
+    async def editbutton_callback(self, button, interaction):
+        await interaction.response.send_modal(editeventname(title="edit the name of the event"))
+        await interaction.delete_original_message()
+    @discord.ui.button(label="Edit date",style=discord.ButtonStyle.primary, custom_id="editeventdate")
+    async def editdatebutton_callback(self, button, interaction):
+        pass
+
+class editeventview(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        global events
+        
+    options=[]
+    now = datetime.now()
+    for event in events:
+        options.append(
+            discord.SelectOption(
+                label=event,
+                description=f'{events[event]} | {(datetime.strptime(events[event], "%d %B, %Y")-now).days} days',
+
+            )
+        )
+
+    @discord.ui.select(placeholder="Select event",options=options)
+    async def select_callback(self, select, interaction):
+        global editevents
+        editevents = select.values[0]
+        await interaction.response.edit_message(view=editevent_())
 
 
 class createmessage(discord.ui.View):
@@ -361,10 +434,12 @@ async def on_ready():
     bot.add_view(channelview())
     bot.add_view(questionchannel())
     bot.add_view(questionchannelconfirm())
+    bot.add_view(yutnoriplayer1view())
 
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def say_hello(ctx):
     await ctx.respond("Hey!")
+    print(ctx.guild.emojis)
 
 
 @bot.slash_command(name="createmessage", description = "create a message as the Bot",) # Create a slash command
@@ -375,6 +450,10 @@ async def create_message(ctx):
 @bot.slash_command(name="addevent", description = "adds an events to the event list",) # Create a slash command
 async def addevent(ctx):
     await ctx.send_modal(addeventmodal(title="add event"))
+
+@bot.slash_command(name="editevent", description = "edits an events from the event list",) # Create a slash command
+async def editevent(ctx):
+    await ctx.respond(view=editeventview())
 
 @bot.slash_command(name="daysuntiljamboree",description="whispers to you the days until the jamboree starts")
 async def DaysUntilJamboree(ctx):
@@ -400,6 +479,100 @@ async def socialscmd(ctx):
         embed.add_field(name=social,value=f'[{social}]({socials[social]})',inline=True)
 
     await ctx.respond(embed=embed,view=socialsview(),ephemeral=True)
+
+############         Yut Nori game         ############
+board = {
+    10:"⚪","gap9":"    ",9:"◽","gap8":"    ",8:"◽","gap7":"    ",7:"◽","gap6":"    ",6:"◽","gap5":"    ",5:"⚪","return5":"\n",
+    "gap10":"\n",
+    11:"◽","gap22":"      ",22:"◽","gap22_":"                    ", 20:"◽","gap4":"      ",4:"◽","return4":"\n",
+    "gap11":"\n",
+    12:"◽","gap23":"            ",23:"◽","gap23_":"        ",21:"◽","gap21":"            ",3:"◽","return3":"\n",
+    "gap12":"                         ",28:"⚪","gap28":"                         ","return28":"\n",
+    13:"◽","gap13":"            ",24:"◽","gap24":"        ",26:"◽","gap26":"            ",2:"◽","return2":"\n",
+    "gap2":"\n",
+    14:"◽","gap14":"      ",25:"◽","gap25":"                    ",27: "◽","gap27":"      ",1:"◽","return1":"\n",
+    "gap1":"\n",
+    15:"⚪","gap15":"    ",16:"◽","gap16":"    ",17:"◽","gap17":"    ",18:"◽","gap18":"    ",19:"◽","gap19":"    ",0:"⚪","startfinish":"<-- START/FINISH",None:'\n',
+    "return6":"\n",
+    "player1home":"◽","gap30":"    ","player2home":"◽","gap31":"                        ","player1finish":"◽","gap32":"    ","player2finish":"◽"
+}
+
+def roll():
+    stick1 = bool(random.getrandbits(1))
+    stick2 = bool(random.getrandbits(1))
+    stick3 = bool(random.getrandbits(1))
+    stick4 = bool(random.getrandbits(1))
+    return stick1, stick2, stick3, stick4
+
+def score(stick1, stick2, stick3, stick4):
+    sticksup = sum([stick1,stick2,stick3,stick4])
+    if sticksup == 0:
+        return 5
+    elif sticksup == 1:
+        return 1
+    elif sticksup == 2:
+        return 2
+    elif sticksup == 3:
+        return 3
+    else:
+        return 4
+
+async def playyutnori(ctx,player1,player2,board,boardstring):
+    await ctx.respond(f'{ctx.author.mention} has started a game of yut nori with {player2.mention}')
+    await ctx.send(boardstring,view=yutnoriplayer1view())
+
+
+@bot.slash_command(name="yutnori",description="starts a game of yut nori with whoever you ping")
+async def yutnorigame(ctx,opponent: Option(discord.Member, "opponent") ):
+    global player2
+    player2 = opponent
+    global player1
+    player1 = ctx.author
+    boardstring = ""
+    for item in list(board.values()):
+        print(item)
+        boardstring += item 
+    await playyutnori(ctx,player1,player2,board,boardstring)
+    
+    
+class yutnoriplayer1view(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+    
+    @discord.ui.button(label="Throw",custom_id="player1throw")
+    async def throw(self, button, interaction):
+        global board
+        global player1
+        global player2
+        if interaction.user == player2:
+            await interaction.response.send_message('It is not your turn to throw!',ephemeral=True)
+        playerthrow = roll()
+        global playerscore
+        playerscore = score(playerthrow[0],playerthrow[1],playerthrow[2],playerthrow[3])
+        emojis = []
+
+        if playerthrow[0]:
+            emojis.append('<:up_:1014501305814880286>')
+        else:
+            emojis.append('<:down_:1014501304321708093>')
+        if playerthrow[1]:
+            emojis.append('<:up_:1014501305814880286>')
+        else:
+            emojis.append('<:down_:1014501304321708093>')
+        if playerthrow[2]:
+            emojis.append('<:up_:1014501305814880286>')
+        else:
+            emojis.append('<:down_:1014501304321708093>')
+        if playerthrow[3]:
+            emojis.append('<:upcross_:1014501307052216411>')
+        else:
+            emojis.append('<:down_:1014501304321708093>')
+        emojistring = " ".join(emojis)
+        await interaction.response.send_message(emojistring)
+        print(playerthrow, playerscore)
+        
+    
+
 
 
 bot.run(os.getenv('TOKEN'))
